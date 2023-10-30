bbe2a4e02a15124ca0d2fc5c979eb016a88f8057
adikpb <67222969+adikpb@users.noreply.github.com>
2023-10-31 00:44:37 +0530

    minor changes

--- Intruder_Detection/solenoid/control.py
+++ Intruder_Detection/solenoid/control.py
@@ -2,19 +2,30 @@ import traceback
 from queue import Queue
 from threading import Event
 from time import sleep
+import os
+linux = os.uname().sysname == "Linux"
+if linux:
+    import RPIO.GPIO as GPIO
 
 solenoid_running = Event()
 solenoid_running.set()
 
 
 def solenoid_loop(activate: Event, queue: Queue):
-    global solenoid_running
+    global solenoid_running, linux
+    if linux:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(18, GPIO.OUT)
     while solenoid_running.is_set():
         try:
             activate.wait()
             print("Opening Door for:", queue.get())
+            if linux:
+                GPIO.output(18,1)
             sleep(8.5)
             print("Closing Door")
+            if linux:
+                GPIO.output(18,1)
             activate.clear()
         except BaseException as e:
             traceback.print_exception(e)
