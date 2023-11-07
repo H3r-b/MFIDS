20eab84a129cc34bed2d1f9638ba9646268c9e44
Herbert <herbertgeorge20@gmail.com>
2023-11-08 00:06:19 +0530

    debig faluse now

--- authsysproject/authsysproject/settings.py
+++ authsysproject/authsysproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uo=8n^bbtto&)&8*!mab%6au&ngk@t+i!9si)19r^%dbbc#9#r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
